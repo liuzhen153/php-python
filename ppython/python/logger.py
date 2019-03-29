b4315b00e6d5f437a25edc6d21b3c8515060fbfd
liuzhen <liuzhen@bookgoal.com>
2019-03-29 15:22:53 +0800

    beauty code

--- ppython/python/logger.py
+++ ppython/python/logger.py
@@ -6,6 +6,8 @@ from datetime import datetime
 from pytz import timezone
 
 cst_tz = timezone('Asia/Shanghai')  # 时区设置
+
+
 def loginfo(*args, **kwargs):
     '''打印日志
     :param args:string
@@ -18,7 +20,8 @@ def loginfo(*args, **kwargs):
 
     now = datetime.now().replace(tzinfo=cst_tz)
     # 获取上级目录下的log
-    log_dir = os.path.dirname(os.path.dirname(__file__)) + '/log/' + now.strftime('%Y%m%d')
+    log_dir = os.path.dirname(os.path.dirname(
+        __file__)) + '/log/' + now.strftime('%Y%m%d')
     check_dir(log_dir)
     log_file = log_dir + '/' + now.strftime('%H') + '.log'
     # 获取调用方信息
@@ -28,11 +31,12 @@ def loginfo(*args, **kwargs):
         for info in args:
             # print(str(info))
             # 如果带上文件信息 + ' [file - ' + sys_info.f_code.co_filename +  ']\r\n'
-            f.write('[' + now.strftime('%Y-%m-%d %H:%M:%S') + ' line:' + str(sys_info.f_back.f_lineno)
-                    + '] - ' + more_msg + str(info) + ' [func:' + sys_info.f_back.f_code.co_name + ']\r\n')
+            f.write('[' + now.strftime('%Y-%m-%d %H:%M:%S') + ' line:' + str(sys_info.f_back.f_lineno) +
+                    '] - ' + more_msg + str(info) + ' [func:' + sys_info.f_back.f_code.co_name + ']\r\n')
     finally:
         if f:
             f.close()
+
 
 def check_dir(base_dir):
     '''检查和生成路径
--- ppython/python/php_python.py
+++ ppython/python/php_python.py
@@ -10,8 +10,8 @@ import logger
 # -------------------------------------------------
 # 基本配置
 # -------------------------------------------------
-LISTEN_PORT = 21230     #服务侦听端口
-CHARSET = "utf-8"       #设置字符集（和PHP交互的字符集）
+LISTEN_PORT = 21230  # 服务侦听端口
+CHARSET = "utf-8"  # 设置字符集（和PHP交互的字符集）
 
 # -------------------------------------------------
 # 主程序
@@ -19,21 +19,22 @@ CHARSET = "utf-8"       #设置字符集（和PHP交互的字符集）
 # -------------------------------------------------
 if __name__ == '__main__':
 
-    logger.loginfo ("-------------------------------------------")
-    logger.loginfo ("- PPython Service")
-    logger.loginfo ("- Time: %s" % time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())) )
-    logger.loginfo ("-------------------------------------------")
+    logger.loginfo("-------------------------------------------")
+    logger.loginfo("- PPython Service")
+    logger.loginfo("- Time: %s" %
+                   time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+    logger.loginfo("-------------------------------------------")
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #TCP/IP
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP/IP
     sock.bind(('', LISTEN_PORT))
     sock.listen(5)
 
-    logger.loginfo ("Listen port: %d" % LISTEN_PORT)
-    logger.loginfo ("charset: %s" % CHARSET)
-    logger.loginfo ("Server startup...")
+    logger.loginfo("Listen port: %d" % LISTEN_PORT)
+    logger.loginfo("charset: %s" % CHARSET)
+    logger.loginfo("Server startup...")
 
     while 1:
-        connection,address = sock.accept()  #收到一个请求
+        connection, address = sock.accept()  # 收到一个请求
 
         # logger.loginfo ("client's IP:%s, PORT:%d" % address)
 
